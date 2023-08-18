9cbcabaf952fa5bdc9d8303acb2f7b9dea241683
Matheus F. Bernardo <dimmilouco@gmail.com>
2023-08-18 16:44:27 -0300

    the href for buttons

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const btn1 = document.getElementById('btn1');
-    const btn2 = document.getElementById('btn2');
-    const btn3 = document.getElementById('btn3');
-  
-    btn1.addEventListener('click', function () {
-      animateButton(btn1);
-    });
-  
-    btn2.addEventListener('click', function () {
-      animateButton(btn2);
-    });
-  
-    btn3.addEventListener('click', function () {
-      animateButton(btn3);
-    });
-  
-    function animateButton(button) {
-      button.classList.add('animated');
-  
-      setTimeout(() => {
-        button.classList.remove('animated');
-      }, 1000);
-    }
+  const btn1 = document.getElementById('btn1');
+  const btn2 = document.getElementById('btn2');
+  const btn3 = document.getElementById('btn3');
+
+  btn1.addEventListener('click', function () {
+    window.location.href = './checkvalidate/';
   });
-  
+
+  btn2.addEventListener('click', function () {
+    window.location.href = './imprimirp';
+  });
+
+  btn3.addEventListener('click', function () {
+    window.location.href = './imprimirg';
+  });
+});
