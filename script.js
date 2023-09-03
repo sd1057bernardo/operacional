02c26d72c080d88c9b1bd8526310b5aba80324a1
Matheus F. Bernardo <dimmilouco@gmail.com>
2023-09-03 01:02:18 -0300

    teste quaggajs

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("Olá dev.")
+const panel = document.querySelector(".panel")
+
+
+function showpanel(model){
+    
+}
+Quagga.init({
+    inputStream : {
+      name : "Live",
+      type : "LiveStream",
+      target: document.querySelector('.panel')    // Or '#yourElement' (optional)
+    },
+    decoder : {
+      readers : ["code_128_reader"]
+    }
+  }, function(err) {
+      if (err) {
+          console.log(err);
+          return
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+  });
